2a637904f9f9f3194bd916f8cb7e9aca922ae2f4
mohamedmansy316 <mohamedmansy855@gmail.com>
2021-04-29 16:29:01 +0200

    Latest\

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -22,7 +22,6 @@
           <li><a href="{{route('expert-hub')}}"><i class="fas fa-check"></i> Blog</a></li>
         </ul>
       </div>
-<<<<<<< HEAD
       <div class="col-lg-5 col-12 footer-map d-flex">
         <div class="row">
           <div class="col-lg-6 col-md-3 col-sm-12 mt-2">
@@ -57,13 +56,6 @@
             </div>
           </div>
         </div>
-=======
-      <div class="col-lg-4 col-12 footer-map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.7004975044897!2d31.196790115115157!3d30.045449181881974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458413486462911%3A0x945b12fc41fd6e8a!2sThe%20Wood%20Court!5e0!3m2!1sen!2seg!4v1605110935180!5m2!1sen!2seg" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>    </section>
-       
->>>>>>> 0fce89e (add)
-     
     </div>
-
   </div>
 </footer>
