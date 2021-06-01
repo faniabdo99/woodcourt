f0c887248830f143a0aa4c106dacd5ab20cc65be
faniabdo99 <faniabdo99@gmail.com>
2021-06-01 16:56:07 +0200

    Latest

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,6 +5,8 @@
   'ogType' => 'Website'
 ])
 <body>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v10.0" nonce="D1CqeXtD"></script>
     @include('layout.navbar')
     <section class="single-event-article">
       <div class="container">
@@ -18,7 +20,7 @@
             <img class="w-100 mb-3" src="{{$TheArticle->imageSrc}}" alt="{{$TheArticle->title}}">
             <div class="event-content">
               {!! $TheArticle->content !!}
-              <div id="disqus_thread"></div>
+              <div class="fb-comments" data-href="https://thewoodcourt.com" data-width="100%" data-numposts="5000000000000000000000000000000"></div>
             </div>
           </div>
           <div class="col-lg-4 col-12">
@@ -46,24 +48,6 @@
       </div>
     </section>
     @include('layout.scripts')
-    <script>
-      /**
-      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-      *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-      /*
-      var disqus_config = function () {
-      this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-      this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-      };
-      */
-      (function() { // DON'T EDIT BELOW THIS LINE
-      var d = document, s = d.createElement('script');
-      s.src = 'https://the-wood-court.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', +new Date());
-      (d.head || d.body).appendChild(s);
-      })();
-  </script>
-  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     @include('layout.footer')
 </body>
 </html>
