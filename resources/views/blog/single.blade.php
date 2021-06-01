e219075da81278519cbb42849b16ad350e938c1b
faniabdo99 <faniabdo99@gmail.com>
2021-06-01 17:07:11 +0200

    Latest

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,8 +5,8 @@
   'ogType' => 'Website'
 ])
 <body>
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v10.0" nonce="D1CqeXtD"></script>
+  <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&appId=1265405060460063&autoLogAppEvents=1" nonce="2rvG1ypI"></script>
     @include('layout.navbar')
     <section class="single-event-article">
       <div class="container">
@@ -20,7 +20,7 @@
             <img class="w-100 mb-3" src="{{$TheArticle->imageSrc}}" alt="{{$TheArticle->title}}">
             <div class="event-content">
               {!! $TheArticle->content !!}
-              <div class="fb-comments" data-href="https://thewoodcourt.com" data-width="100%" data-numposts="5000000000000000000000000000000"></div>
+              <div class="fb-comments" data-href="https://thewoodcourt.com/blog" data-width="100%" data-numposts="20"></div>
             </div>
           </div>
           <div class="col-lg-4 col-12">
