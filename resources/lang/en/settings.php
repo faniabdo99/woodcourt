18ab394a812216884f0263a2c999731c1d168095
mohamedmansy316 <mohamedmansy855@gmail.com>
2021-08-30 19:47:23 +0200

    fix arabic btn

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -3,10 +3,10 @@
 return [
 
     'direction' => 'ltr' ,
-    'direction_reverse' => 'rtl' ,
+    'direction_reverse' => 'ltr' ,
     'direction_initial' => 'initial' ,
     'text_align' => 'text-left',
-    'text_align_inverse' => 'text-right',
+    'text_align_inverse' => 'text-left',
     'col_11_eng' => 'col-11',
     'col_1_eng' => 'col-1'
 ];
